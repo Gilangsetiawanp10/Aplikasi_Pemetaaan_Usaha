4022d8d6e00d028e7051c0ea947754a3b07887ea
Gilangsetiawan <gilangsetiawan308@gmail.com>
2025-08-29 16:14:42 +0800

    Merubah rute

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('maps.index');
 });
 
-Route::get('/maps', [MapController::class, 'index'])->name('maps.index');
+// Route::get('/maps', [MapController::class, 'index'])->name('maps.index');
