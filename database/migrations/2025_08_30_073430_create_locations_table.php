273f268bd1ddd6ce229da0e54310590b81bb9549
difawitsqard <difawitsqard@gmail.com>
2025-08-30 15:35:12 +0700

    location model for storing coordinate locations

---
+++ database/migrations/2025_08_30_073430_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('province', 100)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('district', 100)->nullable();
+            $table->string('village', 100)->nullable();
+            // longitude dan latitude
+            $table->decimal('latitude', 11, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+
+            // unique kecamatan dan desa
+            $table->unique(['district', 'village', 'city', 'province']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
